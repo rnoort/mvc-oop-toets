1e6f1e601e363e90279631eeaa6b77fee2e3d5dc
Robijn Noort <rrnoort@gmail.com>
2022-10-17 11:42:56 +0200

    Titels aangepast en update weggehaald

--- app/controllers/RichestPeoples.php
+++ app/controllers/RichestPeoples.php
@@ -41,25 +41,5 @@
             ];
             $this->view("richestpeoples/delete", $data);
         }
-
-        public function update($id = null)
-        {
-            if($_SERVER["REQUEST_METHOD"] == "POST")
-            {
-                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                $this->countryModel->updateCountry($_POST);
-            }
-                else
-            {
-                $row = $this->countryModel->getSingleCountry($id);
-
-                $data = [
-                    "title" => "<h1>Bijwerken land</h1>",
-                    "row" => $row
-                ];
-
-                $this->view("countries/update", $data);
-            }
-        }
     }
 ?>
